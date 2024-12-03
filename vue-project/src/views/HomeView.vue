6cebb13abe717156f6f6ceeff02025bd98fcf966
filipe moço <filipemoco55@gmail.com>
2024-12-03 22:26:01 +0000

    continuaçao da home

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -3,11 +3,21 @@ import Navbar from '@/components/NavBar.vue';
 </script>
 
 <template>
-  <Navbar />
+  <Navbar/>
+  
+   <div class="event-card">
+     <div class="event-image">
+       <img src="/src/assets/foto exemplo.png" alt=Event Background>
+       <div class="event-overlay">
+         <h2 class="event-title">Come watch Coldplay and much more this summer</h2>
+         <button class="buy-button">BUY NOW</button>
+       </div>
+     </div>
+   </div>
+   <div><h1 class="upcoming-artists">More Upcoming Artists</h1> 
+   </div>
 
-  <h1>
-    Home
-  </h1>
+ 
 
   <footer class="footer">
     <div class="footer-content">
@@ -46,9 +56,13 @@ import Navbar from '@/components/NavBar.vue';
   </footer>
 </template>
 
-<script setup>
 
-</script>
+ <script>
+ export default {
+   name: "EventCard",
+ };
+ </script>
+
 
 <style>
 h1 {
@@ -67,6 +81,76 @@ h3 {
   font-weight: bold;
   font-size: 40px;
 }
+
+.event-card {
+    text-align: center;
+    font-weight: Bold;
+    color: white;
+    background-color: white; 
+    padding: 5px;
+    border-radius: 10px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    max-width: 1200px; 
+    margin: 10px auto; 
+  }
+  
+  .event-image {
+    position: relative;
+    border-radius: 10px;
+    overflow: hidden;
+  }
+  
+  .event-image img {
+    width: 100%;
+    display: block; 
+    height: auto;
+  }
+  
+  .event-overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: rgba(0, 0, 0, 0.3); 
+    color: white;
+    text-align: justify;
+  }
+  
+  .event-title {
+    font-size: 50px;
+    margin-bottom: 320px;
+    line-height: 3; 
+  }
+  
+  .event-details {
+    font-size: 1em;
+    margin-bottom: 20px;
+  }
+  
+  .buy-button {
+    
+    background-color: #0088ccb9;
+    color: rgb(255, 255, 255);
+    border: none;
+    padding: 15px 50px;
+    border-radius: 20px;
+    cursor: pointer;
+    font-size: 15px;
+    font-weight: medium;
+    transition: background-color 0.8s ease;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+    float: right;
+  }
+  
+  .buy-button:hover {
+    background-color: #003b57;
+    opacity: 0.7
+  }
 
 .footer {
   background-color: #004f6f;
